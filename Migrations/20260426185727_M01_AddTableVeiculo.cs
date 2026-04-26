52078660f0c94319029d5ad28a6ac77745751f82
Gustavocodax <gustavodrive2023@gmail.com>
2026-04-26 16:05:07 -0300

    Configura banco de dados e e tabela Veículo

---
+++ Migrations/20260426185727_M01_AddTableVeiculo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuProjeto.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01_AddTableVeiculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Placa = table.Column<string>(type: "TEXT", nullable: false),
+                    AnoFabricacao = table.Column<int>(type: "INTEGER", nullable: false),
+                    AnoModelo = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
--- obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb42fe0135ec521bee2452d001edcd33cc5dc71f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
